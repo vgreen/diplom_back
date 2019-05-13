abea2485b727ab696877ae1fe0ea1c426916ef02
KOLESNIKOV Vladislav <Vladislav.KOLESNIKOV@raiffeisen.ru>
2019-05-13 19:13:16 +0300

    so sad

--- src/controllers/Setter.ts
+++ src/controllers/Setter.ts
@@ -1,4 +1,8 @@
 import {getConnection} from "typeorm";
+import {users} from "../data/users";
+import {stat} from "../data/stat"
+import {Patient, Statistic, User} from "../entity";
+import {patients} from "../data/patients";
 
 export const setNewBasicField = async( Entity:Function , Params:any) => {
     try{
@@ -26,4 +30,37 @@ export const setNewRelatedField = async( Entity:Function , Params:any) => {
     catch(error){
         console.log(error.message);
     }
-}
+};
+
+export const setUsers = () => {
+    users.forEach((item)=>{
+        setNewBasicField(User,item)
+    })
+};
+
+export const setPatients = () => {
+    patients.forEach((item)=>{
+        setNewBasicField(Patient,item)
+    })
+};
+
+export const setStat = () => {
+    stat.forEach(async(item)=>{
+        try{
+            // await getConnection()
+            //     .createQueryBuilder()
+            //     .insert()
+            //     .into(Statistic)
+            //     .values({
+            //         shBekaPost:item.SHkala_Beka_pri_postuplenii,
+            //         shBekaVip:item.SB_na_moment_vipiski,
+            //
+            //     })
+            //     .execute();
+        }
+        catch(error){
+            console.log(error.message);
+        }
+    })
+};
+
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import {createConnection, getRepository} from "typeorm";
 import {User, SpecDiagnose, MainDiagnose, Statistic, Visit, Income, Patient} from "./entity";
 import {Request, Response} from "express";
 import * as express from "express";
-import { setNewField } from "./controllers/Setter";
+import {setNewBasicField, setPatients, setUsers} from "./controllers/Setter";
 import { data } from "./migration/data";
-
 createConnection().then(async connection => {
     
     
@@ -14,30 +13,37 @@ createConnection().then(async connection => {
     const server = express();
     server.use(cors());
     server.use(express.json())
-    
-    const users = await connection.manager.find(User);
-
-    setNewField(MainDiagnose, data.diag);
-
-    console.log("Loading users from the database...");
-    console.log("Loaded users: ", users);
 
 
-    server.get('/', async (request: Request, response: Response) => {
-        const diag = await getRepository(MainDiagnose)
-            .createQueryBuilder("diagnose")
-            .select("diagnose")
-            .leftJoinAndSelect("diagnose.author", "author")
-            .getMany();
-        console.log(diag)
+    //setNewBasicField(MainDiagnose, data.diag);
 
-        response.send('Hello' + diag[3].author.firstName);
-    })
-    server.post("/test", function (request, response) {
-        console.log(request.body);
-        if(!request.body) return response.sendStatus(400);
-        response.send(request.body);
-    });
+    let stat_str = require('fs').readFileSync('/data/stat.txt', 'utf-8').toString();
+    let stat:any;
+    const parse = require('csv-parse')
+    parse(stat_str.trim(), {
+                columns: ["rid","SHkala_Beka_pri_postuplenii","SB_na_moment_vipiski","Situatsionnaya_trevoga_pri_postuplenii","ST_Na_moment_vipiski","Lichnostnaya_trevoga_pri_postuplenii","LT_na_moment_vipiski","SHkala_Gamiltona_pri_postuplenii","SHG_na_moment_vipiski"
+                ]
+            }, function(err, records){
+                stat = records;
+            });
+
+    console.log(stat_str[10]);
+
+    // server.get('/', async (request: Request, response: Response) => {
+    //     const diag = await getRepository(MainDiagnose)
+    //         .createQueryBuilder("diagnose")
+    //         .select("diagnose")
+    //         .leftJoinAndSelect("diagnose.author", "author")
+    //         .getMany();
+    //     console.log(diag)
+    //
+    //     response.send('Hello' + diag[3].author.firstName);
+    // })
+    // server.post("/test", function (request, response) {
+    //     console.log(request.body);
+    //     if(!request.body) return response.sendStatus(400);
+    //     response.send(request.body);
+    // });
 
     server.listen(3001);
     
