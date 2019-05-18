ce21782e7b5774a577f8b3bed3f036cfd649193a
KOLESNIKOV Vladislav <Vladislav.KOLESNIKOV@raiffeisen.ru>
2019-05-18 21:48:39 +0300

    so sad

---
+++ src/entities/mgerm/index.ts
@@ -0,0 +1,6 @@
+import { epm_records } from './epm_records'
+import {patients_list} from "./patients_list";
+import {department_income} from "./department_income";
+import {department_outcome} from "./department_outcome";
+
+export { epm_records, patients_list, department_outcome, department_income };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import {createConnection, getRepository} from "typeorm";
 import {User, SpecDiagnose, MainDiagnose, Statistic, Visit, Income, Patient} from "./entities/spec_db";
+import { epm_records } from './entities/mgerm'
 import {Request, Response} from "express";
 import * as express from "express";
 import {setNewBasicField, setPatients, setUsers} from "./controllers/Setter";
@@ -17,28 +18,17 @@ createConnection().then(async connection => {
 
     //setNewBasicField(MainDiagnose, data.diag);
 
-    let stat_str = require('fs').readFileSync('/data/stat.txt', 'utf-8').toString();
-    let stat:any;
-    const parse = require('csv-parse')
-    parse(stat_str.trim(), {
-                columns: ["rid","SHkala_Beka_pri_postuplenii","SB_na_moment_vipiski","Situatsionnaya_trevoga_pri_postuplenii","ST_Na_moment_vipiski","Lichnostnaya_trevoga_pri_postuplenii","LT_na_moment_vipiski","SHkala_Gamiltona_pri_postuplenii","SHG_na_moment_vipiski"
-                ]
-            }, function(err, records){
-                stat = records;
-            });
 
-    console.log(stat_str[10]);
+    server.get('/', async (request: Request, response: Response) => {
+        const diag = await getRepository(MainDiagnose)
+            .createQueryBuilder("diagnose")
+            .select("diagnose")
+            .leftJoinAndSelect("diagnose.author", "author")
+            .getMany();
+        console.log(diag)
 
-    // server.get('/', async (request: Request, response: Response) => {
-    //     const diag = await getRepository(MainDiagnose)
-    //         .createQueryBuilder("diagnose")
-    //         .select("diagnose")
-    //         .leftJoinAndSelect("diagnose.author", "author")
-    //         .getMany();
-    //     console.log(diag)
-    //
-    //     response.send('Hello' + diag[3].author.firstName);
-    // })
+        response.send('Hello' + diag[3].author.firstName);
+    })
     // server.post("/test", function (request, response) {
     //     console.log(request.body);
     //     if(!request.body) return response.sendStatus(400);
