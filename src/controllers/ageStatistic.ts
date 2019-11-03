4db7eea82bafb8a28543ebdf28d55e4a283438c8
v.kolesnikov <v.kolesnikov@digitalcity.group>
2019-11-03 14:01:18 +0300

    age statistic works!!!!

--- src/controllers/ageStatistic.ts
+++ src/controllers/ageStatistic.ts
@@ -11,6 +11,7 @@ async function ageStatisticRequest(request: Request, response: Response) {
     try {
         ageStatistic = await getAgeStatistic(request.body.dateStart, request.body.dateEnd, request.body.dep);
         console.log(request.body);
+        console.log(ageStatistic);
         response.send(ageStatistic)
 
     } catch (error) {
@@ -25,65 +26,35 @@ export const getAgeStatistic = async (dateStart: string, dateEnd: string, depart
         dateE:string = dateEnd.toString();
     try {
         return await getConnection("mgerm_connect")
-            .query("SELECT di.department, count(CASE\n" +
-                "    WHEN di.age < 18 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as 'under18' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 18 and di.age < 22 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '18-21' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 22 and di.age < 25 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '22-24' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 25 and di.age < 30 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '25-29' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 30 and di.age < 35 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '30-34' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 35 and di.age < 40 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '35-39' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 40 and di.age < 45 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '40-44' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 45 and di.age < 50 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '45-49' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 50 and di.age < 55 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '50-54' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 55 and di.age < 60 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '55-59' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 60 and di.age < 65 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '60-64' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 65 and di.age < 70 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '65-69' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >= 70 and di.age < 75 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '70-74' ,\n" +
-                "count(CASE\n" +
-                "    WHEN di.age >75 THEN 1\n" +
-                "    ELSE NULL\n" +
-                "END) as '75+' \n" +
-                "FROM mgerm.department_income as di\n" +
-                "where di.date >= '"+ dateS +"' and di.date < '" + dateE + "' " + dep +
-                "group by department\n");
+            .query("SELECT department,\n" +
+                "\t\t\n" +
+                "\t\tCOUNT(IF(age<=17,1,null)) as 'under18',\n" +
+                "\t\tCOUNT(IF(age between 18 and 19,1,null)) as '19',\n" +
+                "\n" +
+                "\t\tCOUNT(IF(age between 20 and 24,1,null)) as '20-24',\n" +
+                "\t\tCOUNT(IF(age between 25 and 29,1,null)) as '25-29',\n" +
+                "\t\tCOUNT(IF(age between 30 and 34,1,null)) as '30-34',\n" +
+                "\t\tCOUNT(IF(age between 35 and 39,1,null)) as '35-39',\n" +
+                "\t\tCOUNT(IF(age between 40 and 44,1,null)) as '40-44',\n" +
+                "\t\tCOUNT(IF(age between 45 and 49,1,null)) as '45-49',\n" +
+                "\t\tCOUNT(IF(age between 50 and 54,1,null)) as '50-54',\n" +
+                "\t\tCOUNT(IF(age between 55 and 59,1,null)) as '55-59',\n" +
+                "\t\t\n" +
+                "\t\tCOUNT(IF(age between 60 and 69,1,null)) as '60+',\n" +
+                "\t\tCOUNT(IF(age>=70,1,null)) as '70+'\n" +
+                "\n" +
+                "\t\tFROM\n" +
+                "\t\t(\n" +
+                "\t\tSELECT \n" +
+                "\t\t\tt1.hystoryNumber,\n" +
+                "\t\t\tt1.age,\n" +
+                "\t\t\tIF(t1.department='Дн. ст.','Дн. ст.',concat(t1.department)) as department\n" +
+                "\t\tFROM mgerm.department_income as t1\n" +
+                "\t\tWHERE t1.date >= '"+ dateStart +"' and t1.date <'"+dateEnd+"'\n" +
+                dep +
+                "\t\t) as tabl1\n" +
+                "\t\tGROUP BY department\n" +
+                "\t\tORDER BY CAST(department  AS SIGNED)\n");
 
     } catch (error) {
         console.log(error.message);
