e2bed587370685a524f27de7aa60dc5ad60b9cef
Vlad Kolesnikov <v.kolesnickoff2012@yandex.ru>
2020-02-24 14:37:32 +0200

    Update patTablePsy.ts
    fix rid

--- src/queries/patTablePsy.ts
+++ src/queries/patTablePsy.ts
@@ -2,9 +2,9 @@ export const generateTablePsy = (dateStart:string = '01-01-2018', dateEnd: strin
     `
 SELECT distinct pl.last_name, pl.first_name, pl.second_name, in_.hystoryNumber as 'istoria_bolezn' , date_format(in_.date,' %d-%m-%Y') as 'postupil', date_format(out_.out_date,' %d-%m-%Y') as 'vibil', t.* FROM vmh_db.otsenka_psihologicheskogo_sostoyaniya_bolnogo as t
 left join ( 
-    select formalisedDataID,  patientID, hystoryNumber from mgerm.epm_records where recordTypeID in (690, 680) and deleted = 0 and digest is NOT NULL and incorrect=0
+    select formalisedDataID,  patientID, hystoryNumber from mgerm.epm_records where recordTypeID in (680) and deleted = 0 and digest is NOT NULL and incorrect=0
 \t  union all
-\t  select formalisedDataID,  patientID, hystoryNumber from mgerm_archive.epm_records where recordTypeID in (690, 680) and deleted = 0 and digest is NOT NULL and incorrect=0) t2 on t2.formalisedDataID = t.rid
+\t  select formalisedDataID,  patientID, hystoryNumber from mgerm_archive.epm_records where recordTypeID in (680) and deleted = 0 and digest is NOT NULL and incorrect=0) t2 on t2.formalisedDataID = t.rid
 left join mgerm.department_income as in_ on in_.hystoryNumber = t2.hystoryNumber
 left join mgerm.department_outcome as out_ on out_.hystoryNumber = in_.hystoryNumber
 left join mgerm.patients_list as pl on in_.patientID = pl.patientID
